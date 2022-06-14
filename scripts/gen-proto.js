b5b6a15883dbb68b58e161cad3193572b914569a
ruslan glaznyov <ruslan.glaznyov@gmail.com>
2022-06-14 20:28:55 +0300

    telescope setup

--- scripts/gen-proto.js
+++ scripts/gen-proto.js
@@ -1,7 +1,10 @@
 import { join } from 'path';
 import telescope from '@osmonauts/telescope';
 
-const protoDirs = [join(__dirname, '/../chain/proto')];
+const protoDirs = [
+  join(__dirname, '/../proto'),
+  join(__dirname, '/../chain/proto')
+];
 const outPath = join(__dirname, '../src/proto');
 
 telescope({
