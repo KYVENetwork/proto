67385bc29c67c2f1402aa96903c5d2b3ec5856c6
ruslan glaznyov <ruslan.glaznyov@gmail.com>
2022-06-16 10:57:55 +0300

    include cosmos types; generate all 64 numbers as string

--- proto-gen.sh
+++ proto-gen.sh
@@ -7,7 +7,7 @@ KYVE_PROTO='./chain/proto'
 protoc --plugin="protoc-gen-ts_proto=${PROTOC_GEN_TS_PROTO_PATH}" \
 --ts_proto_out="${OUT_DIR}" \
 --ts_proto_out="${PROTO_DIR}" \
---ts_proto_opt="esModuleInterop=true,forceLong=string,useOptionals=true" \
+--ts_proto_opt="esModuleInterop=true,forceLong=string,useOptionals=messages" \
 --proto_path="$PROTO_DIR" \
 --proto_path="$KYVE_PROTO" \
 $(find ${KYVE_PROTO}  -path -prune -o -name '*.proto' -print0 | xargs -0)
